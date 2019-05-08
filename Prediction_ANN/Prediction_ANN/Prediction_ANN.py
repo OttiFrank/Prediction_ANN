1e0c6b1361dc55cd09766b916e2d73a161b35eb7
Martin Leijon <martinleeijon@gmail.com>
2019-05-08 13:23:42 +0200

    Updated

--- Prediction_ANN/Prediction_ANN/Prediction_ANN.py
+++ Prediction_ANN/Prediction_ANN/Prediction_ANN.py
@@ -2,6 +2,30 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.models import model_from_json
+from sklearn.preprocessing import MinMaxScaler
+
+def series_to_supervised(values, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(values) is list else values.shape[1]
+    df = pd.DataFrame(values)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forcast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1)) for j in range(n_vars)]
+    # PPut it all together
+    agg = pd.concat(cols, axis = 1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan: 
+        agg.dropna(inplace=True)
+    return agg
 
 url='http://users.du.se/~h16wilwi/gik258/data/ANN-interpolerad.xlsx'
 dataset = pd.read_excel(url, skiprows=3)
@@ -35,13 +59,29 @@ print("Loaded model from disk")
 
 prediction_list = list()
 
+n_days = 3
+n_features = 9
+n_obs = n_days * n_features
+
 for index in range(2):
         df = dataset_GP.iloc[:, index]
         df = pd.concat([df, dataset_W], axis=1)
         print('index: {}'.format(index))
 
+        values = df.values.astype('float32')
+        #normalize features
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaled = scaler.fit_transform(values)
+        # frame as supervised learning
+        reframed = series_to_supervised(scaled, n_days,1 )
+
+        test = values[:, :]
+        test = test[:, :n_obs]
+
+        test = test.reshape((test.shape[0], n_days, n_features))
+
         loaded_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-        prediction = loaded_model.predict_proba(df)
+        prediction = loaded_model.predict_proba(test)
         prediction_list.append(prediction)
 prediction_list
         
