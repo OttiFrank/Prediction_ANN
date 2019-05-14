a9e7f7e3ce6b6002b8f448233acee73ae42258ad
William <william.wirsen@live.se>
2019-05-14 15:18:38 +0200

    Update

--- Prediction_ANN/Prediction_ANN/Prediction_ANN.py
+++ Prediction_ANN/Prediction_ANN/Prediction_ANN.py
@@ -27,17 +27,11 @@ dataset = dataset.drop(['Daggp_mean', 'TYtaDaggp_mean'])
 
 # Ground data
 dataset_GP = dataset.iloc[:1159, 927:]
-dataset_GP
-len(dataset_GP)
 # Weather data
 dataset_W = dataset.iloc[1159:, 927:]
-dataset_W
-len(dataset_W)
 # Transpose dataset
 dataset_W = dataset_W.transpose()
 dataset_GP = dataset_GP.transpose()
-dataset_GP
-len(dataset_GP)
 # Convert series to supervised learning
 def series_to_supervised(values, n_in=1, n_out=1, dropnan=True):
     n_vars = 1 if type(values) is list else values.shape[1]
@@ -75,27 +69,28 @@ loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 
 prediction_list = list()
-# For each data point
-len(dataset_GP)
-# from 927, 231 iterations
+# from column 927 (points)
 i = 927
-for index  in range(231):
-        
+# For each data point
+for index  in range(5):        
         df = dataset_GP.iloc[:, i]
-        #df = pd.concat([df,dataset_W], sort=False)
         df = pd.concat([df, dataset_W], axis=1)
+        df
         print('index: {}'.format(i))
         values = df.astype('float64')
-        #normalize features
+        # normalize features
         scaler = MinMaxScaler(feature_range=(0,1))
         scaled = scaler.fit_transform(values)
         # frame as supervised learning
         reframed = series_to_supervised(scaled, n_days,1 )
         values = reframed.values
-        val = values[:, :n_obs]
+        val, val_test = values[:, :n_obs], values[:, -n_features]
         val = val.reshape(val.shape[0], n_days, n_features)
         # Compile ANN
-        loaded_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        #loaded_model.compile(loss='mse', optimizer='adam')
+
+        # Evaluate the model
+        scores = loaded_model.predict(val)
 
         # Make prediction
         prediction = loaded_model.predict_proba(val)
@@ -104,10 +99,12 @@ for index  in range(231):
         
 prediction_list
 pred_list = []
+summary = list()
 len(prediction_list[0])
 for j in range(len(prediction_list)):
     for i in range (len(prediction_list[j])):
         pred_list.append(prediction_list[0][i][0])
+        summary.append((prediction_list[0][i][0] * 100))
 
 my_list = map(lambda x: x[0], pred_list)
 series = pd.Series(pred_list)
@@ -115,56 +112,88 @@ series.hist()
 pyplot.show()
 
 '''
-test_X = test_X.reshape((test_X.shape[0], n_days*n_features))
-# invert scaling for forecast
-inv_yhat = np.concatenate((prediction, test_X[:, -(n_features-1):]), axis=1)
-inv_yhat = scaler.inverse_transform(inv_yhat)
-inv_yhat = inv_yhat[:,0]
-last_predicted = inv_yhat[-1]
+# Plot our points on a map
+url = "http://users.du.se/~h15marle/GIK258_Examensarbete/Data/railway_data.csv"
+all_points = pd.read_csv(url)
+our_points = pd.read_excel('predicted.xlsx', index_col=0)
+our_points = our_points.iloc[:, 0:2]
+all_points = all_points.iloc[:, 1:3]
+all_points
 
-pred_list.append(last_predicted)
-last_predicted
-# invert scaling for actual
-test_y = test_y.reshape((len(test_y), 1))
-inv_y = np.concatenate((test_y, test_X[:, -(n_features-1):]), axis=1)
-inv_y = scaler.inverse_transform(inv_y)
-        
-inv_y = inv_y[:,0]
-last_true_value = inv_y[-1]
-if (index % 5 == 0):
-    print(pred_list)
+all_geometry = [Point(xy) for xy in zip(all_points["pnt_lat"], all_points["pnt_lon"])]
+our_geometry = [Point(xy) for xy in zip(our_points["lat"], our_points["lng"])]
 
+# concat multible shapefiles into one gpd df
+folder = Path("shp")
+print("---- Letar efter och concat av shape-filer ---- ")
+gdf = pd.concat([
+    gpd.read_file(shp)
+    for shp in folder.glob("*.shp")
+], sort=False).pipe(gpd.GeoDataFrame)
 
+print("--- Startar skapandet av GeoDataFrame -----")
+geo_df_all = gpd.GeoDataFrame(all_points,
+                          crs= 'merc',
+                          geometry = all_geometry)
+geo_df_our = gpd.GeoDataFrame(our_points,
+                          crs= 'merc',
+                          geometry = our_geometry)
 
-pred_list = pd.DataFrame([pred_list])
-pred_list = pred_list.transpose()
-pred_list.reset_index(level=0, inplace=True)
-pred_list.set_index('index')
-true_values = pd.DataFrame([dataset.iloc[:1159,-1]])
-true_values = true_values.transpose()
-true_values
+def plot_map():
+    # Limit map size
+    xlim = ([12.030, 12.0475])
+    ylim = ([57.615, 57.640])
+    
+    # dot size
+    dot_size = 1
 
-df = pd.concat([true_values, pred_list], axis=1, sort=False)
-lat_long_only
-result = pd.concat([lat_long_only, df], axis=1, sort=False)
-result
-result = result.drop(['index'], axis=1)
-result.columns = ["lat", "lon", 'true values', 'predicted values']
-#result = result.rename(index=str, columns={result.columns: 'True values', 0 : 'Predicted'})
-print(result)
+    fig, (ax1, ax2) = pyplot.subplots(1,2, sharey=True, figsize=(15,15))
 
+    ax1.set_xlim(xlim)
+    ax1.set_ylim(ylim)
+    ax2.set_xlim(xlim)
+    ax2.set_ylim(ylim)
+    ax1.set_title('All points', fontsize='xx-large')
+    ax2.set_title('Our points', fontsize='xx-large')
+    print("--- Plottar ut första kartan ----")
+    gdf.plot(ax = ax1, alpha=0.8, zorder=0)
+    print("--- Plottar ut andra kartan ----")
+    gdf.plot(ax = ax2, alpha=0.8, zorder=0)    
+    geo_df_all
+    geo_df_all['geometry'].plot(ax = ax1, markersize = dot_size, color = 'gold', marker = "o", zorder=6)
+    geo_df_our['geometry'].plot(ax = ax2, markersize = dot_size, color = 'gold', marker = "o", zorder=6)
+    pyplot.show()
 
-# writer = ExcelWriter('predicted.xlsx', engine='xlsxwriter')
-# writer.book.use_zip64()
-# result.to_excel(writer, sheet_name="Blad1")
-# writer.save()
+plot_map()
 '''
-'''
+
+
+
 df = pd.read_excel('predicted.xlsx', index_col=0)
+df
 df_true, df_pred = df, df
-df_true.drop('predicted values', axis=1)
-df_pred.drop('true values', axis=1)
-df_true
+df_true = df_true.drop('predicted values', axis=1)
+df_pred = df_pred.drop('true values', axis=1)
+
+absolute_values = list()
+counter= 0;
+for i in range(len(df_pred)): 
+    pred, true = df_pred.iloc[i,2], df_true.iloc[i,2]
+    df_pred.iloc[0,2]
+    df_true.iloc[0,2]
+    if pred > true:
+        precentage = (+((pred-true)/true))   
+        counter += 1
+    else:
+        percentage = (+((true-pred)/true))   
+    absolute_values.append(percentage)
+counter
+absolute_values
+
+my_list = pd.Series(absolute_values)
+my_list = my_list.replace(-np.inf, 0)
+my_list.hist()
+pyplot.show()
 
 # Plot points on map
 geometry = [Point(xy) for xy in zip(df["lng"], df["lat"])]
@@ -191,7 +220,7 @@ def plot_map():
     ylim = ([57.615, 57.640])
     
     # dot size
-    dot_size = 1
+    dot_size = 6
 
     fig, (ax1, ax2) = pyplot.subplots(1,2, sharey=True, figsize=(15,15))
 
@@ -208,9 +237,12 @@ def plot_map():
 
     print("---- Påbörjar uträkningarna av plottar -----")
     # Points for ax1
-    geo_df_true[geo_df_true['true values'] <= 2].plot(ax = ax1, markersize = dot_size, color = 'gold', label="Ingen höjning", marker = "o", zorder=6)
+    geo_df_true[(geo_df_true['true values'] >= -0.001) & (geo_df_true['true values'] < 0)].plot(ax = ax1, markersize = dot_size, color = 'maroon', marker = "*", label="-1mm < 0mm", zorder=6)
+    geo_df_true[(geo_df_true['true values'] >= 0) & (geo_df_true['true values'] <= 0.001)].plot(ax = ax1, markersize = dot_size, color = 'gold', marker = "*", label="0mm - 1mm", zorder=6)
     # Points for ax2
-    #geo_df[(geo_df['predicted values'] > 0)].plot(ax = ax2, markersize = dot_size, color = 'green', marker = "o", label = "Högst höjning", zorder=6)
+    geo_df_pred[(geo_df_pred['predicted values'] >= -0.001) & (geo_df_pred['predicted values'] < 0)].plot(ax = ax2, markersize = dot_size, color = 'maroon', marker = "*", label="-1mm < 0mm", zorder=6)
+    geo_df_pred[(geo_df_pred['predicted values'] >= 0) & (geo_df_pred['predicted values'] <= 0.001)].plot(ax = ax2, markersize = dot_size, color = 'gold', marker = "*", label="0mm - 1mm", zorder=6)
+    '''
     geo_df_pred[(geo_df_true['true values'] - geo_df_pred['predicted values'] <= -0.001)].plot(
                     ax = ax2, 
                     markersize = dot_size, 
@@ -219,6 +251,7 @@ def plot_map():
                     label = "Felmarginal på 1.5mm (+)", 
                     zorder=6)
     print("---- Påbörjar uträkningarna andra plotten -----")
+   
     geo_df_pred[(geo_df_true['true values'] - geo_df_pred['predicted values'] > -0.001) & 
                 (geo_df_true['true values'] - geo_df_pred['predicted values'] < -0.0002)].plot(
                     ax = ax2, 
@@ -249,7 +282,7 @@ def plot_map():
                     marker = "o", 
                     label = "Felmarginal under 0.1mm (-)", 
                     zorder=6)
-
+    '''
     #geo_df[(geo_df['predicted values'] > -0.02) & (geo_df['predicted values'] < 0)].plot(ax = ax2, markersize = dot_size, color = 'yellow', marker = "o", label = "Mindre sättning", zorder=4)
     #geo_df[(geo_df['predicted values'] > -0.09) & (geo_df['predicted values'] <= -0.02)].plot(ax = ax2, markersize = dot_size, color = 'orange', marker = "o", label = "Medel sättning", zorder=5)
     #geo_df[(geo_df['predicted values'] <= -0.021)].plot(ax = ax2, markersize = dot_size, color = 'red', marker = "^", label = "Högst sättning", zorder=6)
@@ -259,11 +292,7 @@ def plot_map():
 
 plot_map()
 
-# street = gpd.read_file("shape/Exjobb.shp")
-# street = gpd.GeoDataFrame(street)
-# fig, ax = pyplot.subplots(figsize = (15,15))
-# street.plot(ax = ax)
-# pyplot.show()
-'''       
+
+    
 
 
