a4c4d1faa823e1bfe756053c862f10d8402d74e7
William <william.wirsen@live.se>
2019-05-15 16:29:17 +0200

    Added prediction for specific InSAR-point over time

--- Prediction_ANN/Prediction_ANN/Prediction_ANN.py
+++ Prediction_ANN/Prediction_ANN/Prediction_ANN.py
@@ -246,7 +246,12 @@ pyplot.show()
 # END
 
 # Plot points on map
-geometry = [Point(xy) for xy in zip(df["lng"], df["lat"])]
+df = pd.read_excel('predicted.xlsx', index_col=0)
+df
+df_true, df_pred = df, df
+df_true = df_true.drop('predicted values', axis=1)
+df_pred = df_pred.drop('true values', axis=1)
+geometry = [Point(xy) for xy in zip(df["lon"], df["lat"])]
 
 # concat multible shapefiles into one gpd df
 folder = Path("shp")
@@ -288,14 +293,146 @@ def plot_map():
     print("---- Påbörjar uträkningarna av plottar -----")
     # Points for ax1
     geo_df_true[(geo_df_true['true values'] > 0.001)].plot(ax = ax1, markersize = dot_size, color = 'blue', marker = "*", label="> 1mm", zorder=6)
-    geo_df_true[(geo_df_true['true values'] >= -0.001) & (geo_df_true['true values'] <= 0.001)].plot(ax = ax1, markersize = dot_size, color = 'forestgreen', marker = "*", label="-1mm - 1mm", zorder=5)
-    geo_df_true[(geo_df_true['true values'] < -0.001)].plot(ax = ax1, markersize = dot_size, color = 'maroon', marker = "*", label="< -1mm", zorder=4)
+    geo_df_true[(geo_df_true['true values'] >= -0.001) & (geo_df_true['true values'] <= 0.001)].plot(ax = ax1, markersize = dot_size, color = 'green', marker = "*", label="-1mm - 1mm", zorder=5)
+    geo_df_true[(geo_df_true['true values'] < -0.001)].plot(ax = ax1, markersize = dot_size, color = 'red', marker = "*", label="< -1mm", zorder=4)
     # Points for ax2
-    geo_df_pred[(geo_df_pred['predicted values'] > 0.001)].plot(ax = ax2, markersize = dot_size, color = 'forestgreen', marker = "*", label="> 1mm", zorder=6)
-    geo_df_pred[(geo_df_pred['predicted values'] >= -0.001) & (geo_df_pred['predicted values'] <= 0.001)].plot(ax = ax2, markersize = dot_size, color = 'gold', marker = "*", label="-1mm - 1mm", zorder=5)
-    geo_df_pred[(geo_df_pred['predicted values'] < -0.001)].plot(ax = ax2, markersize = dot_size, color = 'maroon', marker = "*", label="< -1mm", zorder=4)
+    geo_df_pred[(geo_df_pred['predicted values'] > 0.001)].plot(ax = ax2, markersize = dot_size, color = 'blue', marker = "*", label="> 1mm", zorder=6)
+    geo_df_pred[(geo_df_pred['predicted values'] >= -0.001) & (geo_df_pred['predicted values'] <= 0.001)].plot(ax = ax2, markersize = dot_size, color = 'green', marker = "*", label="-1mm - 1mm", zorder=5)
+    geo_df_pred[(geo_df_pred['predicted values'] < -0.001)].plot(ax = ax2, markersize = dot_size, color = 'red', marker = "*", label="< -1mm", zorder=4)
 
     pyplot.legend(prop={'size': 12})
     pyplot.show()
 
 plot_map()
+
+
+# Plot ground level change + rain sum
+'''
+
+url='http://users.du.se/~h16wilwi/gik258/data/ANN-interpolerad.xlsx'
+dataset = pd.read_excel(url, skiprows=3)
+
+#lat_long_only = dataset[['pnt_lat','pnt_lon']]
+lat_long_only = dataset.iloc[:1159, 2:4]
+lat_long_only
+dataset = dataset.drop(['pnt_id', 'pnt_lat', 'pnt_lon', 'pnt_demheight', 'pnt_height', 'pnt_quality', 'pnt_linear'], axis=1)
+
+dataset.set_index('index', inplace=True)
+dataset = dataset.drop(['Daggp_mean', 'TYtaDaggp_mean'])
+
+# Ground data
+dataset_GP = dataset.iloc[199, :]
+dataset_GP
+# Weather data
+dataset_W = dataset.iloc[1159:, :]
+dataset_W
+# Transpose dataset
+dataset_W = dataset_W.transpose()
+dataset_GP = dataset_GP.transpose()
+# Convert series to supervised learning
+def series_to_supervised(values, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(values) is list else values.shape[1]
+    df = pd.DataFrame(values)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forcast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1)) for j in range(n_vars)]
+    # PPut it all together
+    agg = pd.concat(cols, axis = 1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan: 
+        agg.dropna(inplace=True)
+    return agg
+
+n_days = 1
+n_features = 11
+n_obs = n_days * n_features
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# from column 927 (points)
+
+len(dataset_GP)
+# For one data point      
+df = dataset_GP
+dataset_W
+df = pd.concat([df, dataset_W], axis=1, sort=False)
+df
+values = df.astype('float64')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, n_days,1 )
+values = reframed.values
+val, val_test = values[:, :n_obs], values[:, -n_features]
+val = val.reshape(val.shape[0], n_days, n_features)
+# Compile ANN
+#loaded_model.compile(loss='mse', optimizer='adam')
+
+# Evaluate the model
+scores = loaded_model.predict(val)
+
+# Make prediction
+prediction = loaded_model.predict(val)
+test_X = val.reshape((val.shape[0], n_days*n_features))
+# invert scaling for forecast
+inv_yhat = np.concatenate((prediction, test_X[:, -(n_features-1):]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+inv_yhat
+#last_predicted = inv_yhat[-1]
+# invert scaling for actual
+test_y = val_test.reshape((len(val_test), 1))
+inv_y = np.concatenate((test_y, test_X[:, -(n_features-1):]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+inv_y
+
+
+
+len(inv_yhat)
+inv_yhat
+inv_y
+for i in range(len(inv_yhat)):
+    i
+    inv_yhat[i] = inv_yhat[i] * 1000
+    inv_y[i] = inv_y[i] * 1000
+
+pred_list = pd.DataFrame([inv_yhat, inv_y])
+pred_list = pred_list.transpose()
+pred_list
+pred_list.reset_index(level=0, inplace=True)
+pred_list = pred_list.set_index('index')
+pred_list.columns = ['predicted values', 'actual values']
+rain_values = pd.DataFrame(dataset_W['Rain_mm_sum'])
+rain_values.reset_index(level=0, inplace=True)
+rain_values
+df = pd.merge(rain_values, pred_list, left_index=True, right_index=True)
+df = df.set_index('index')
+
+pyplot.plot(df['predicted values'], color='blue', label='Predikterade värden')
+pyplot.plot(df['actual values'], color='orange', label='Riktiga värden')
+pyplot.plot(df['Rain_mm_sum'], color='forestgreen', label='Regnmängd')
+pyplot.xlabel('Tid')
+pyplot.ylabel('millimeter')
+pyplot.title('Marknivå och regnmängd under en treårsperiod mätt i millimeter')
+pyplot.legend(['Predikterade värden', 'Riktiga värden', 'Regn'])
+pyplot.show()
+
+'''
